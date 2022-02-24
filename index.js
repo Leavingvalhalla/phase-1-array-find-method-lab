5ab209a922deeaff0e5e59432be4120a3c5eb2c6
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-23 17:32:56 -0800

    commit tree a9352546483e836ab089693250cf56bf8c495252

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 function superbowlWin(arr) {
   const element = arr.find(isWin);
-  //   if (element) {
-  //     return element.year;
-  //   }
-  //     return element.year;
-  //   element ? element.year : undefined;
-  element && element.year;
+  return element && element.year;
 }
 
 const isWin = (element) => element['result'] === 'W';
