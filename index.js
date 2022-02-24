fa31b82917ed8965859d08559d8daefd59620f98
Zach Lefler <leaving.valhalla@gmail.com>
2022-02-23 17:29:55 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(arr) {
+  const element = arr.find(isWin);
+  //   if (element) {
+  //     return element.year;
+  //   }
+  //     return element.year;
+  //   element ? element.year : undefined;
+  element && element.year;
+}
+
+const isWin = (element) => element['result'] === 'W';
